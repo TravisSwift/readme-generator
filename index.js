e9e1a277d19cc6efbf8863d8c018deab206af1be
Travis Swift <travis.arthur.swift@gmail.com>
2022-04-06 10:50:11 -0500

    adding html output

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
-var message = 'Hello Node!';
+// const fs = require('fs');
+// const inquirer = require('inquirer');
+// // const readline = require('readline');
+// //const generatePage = require('./src/page-template');
 
-var sum = 5 + 3;
 
-console.log(message);
-console.log(sum);
+// var message = 'Hello Node!';
+// console.log(message);
+ 
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
+
+// const readline = require('npm install').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   })
   
-  readline.question(`What's your name?`, name => {
-    console.log(`Hi ${name}!`)
-    readline.close()
-  })
+//   readline.question(`What's your name?`, name => {
+//     console.log(`Hi ${name}!`)
+//     readline.close()
+//   })
+
+//   const readmeDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(readmeDataArgs);
+
+// // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printReadmeData = readmeDataArr => {
+//     for (let i = 0; i < readmeDataArr.length; i++) {
+//       console.log(readmeDataArr[i]);
+//     }
+//   };
+
+//   printReadmeData(readmeDataArgs);
+
+
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+
+
+console.log(name, github);
+console.log(generatePage(name, github));
+ 
